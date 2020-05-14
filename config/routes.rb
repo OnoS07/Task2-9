88648c74b7e781400636882ee63cf9a59557318c
Ono <soichrooono@gmail.com>
2020-05-14 06:43:40 +0000

    [Add]header navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "users#home"
+  get "/" => "users#home"
+  get "/home/about" => "users#about"
+
 end
