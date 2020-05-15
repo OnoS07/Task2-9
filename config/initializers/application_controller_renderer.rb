5ef845002c801b5996f628fc7aabb88ea06ade01
Ono <soichrooono@gmail.com>
2020-05-15 01:40:59 +0000

    [Add]User/show.edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0af6b2d2421fd61c5bf572a37fad2ce112e3914e348f54381828bdd0a800ef48601a65f4f89fbf94c6ce93dff0ecb72a06a3ce4b65c3e331d915c90192dacf3e'
